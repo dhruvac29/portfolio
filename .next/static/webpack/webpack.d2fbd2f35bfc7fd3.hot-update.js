c175398961e1bc0c24c859da824a16b8ba417266
dhruvac29 <dhruvac29@gmail.com>
2024-11-25 12:09:16 -0600

    Analytics added

--- .next/static/webpack/webpack.d2fbd2f35bfc7fd3.hot-update.js
+++ .next/static/webpack/webpack.d2fbd2f35bfc7fd3.hot-update.js
@@ -11,8 +11,21 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "29693931be0e03db"; }
+/******/ 	__webpack_require__.h = function() { return "d79a07a1b0da1f8d"; }
 /******/ }();
+/******/ 
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ 
+/******/ // noop fns to prevent runtime errors during initialization
+/******/ if (typeof self !== "undefined") {
+/******/ 	self.$RefreshReg$ = function () {};
+/******/ 	self.$RefreshSig$ = function () {
+/******/ 		return function (type) {
+/******/ 			return type;
+/******/ 		};
+/******/ 	};
+/******/ }
 /******/ 
 /******/ }
 );
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { ThemeProvider } from 'next-themes';
+import { Analytics } from "@vercel/analytics/react";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
