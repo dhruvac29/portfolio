fdc6ac33329604c3fc6f3801de9364baf2c7f10f
dhruvac29 <dhruvac29@gmail.com>
2024-11-25 12:14:08 -0600

    deployment error solved

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import { ThemeProvider } from 'next-themes';
-import { Analytics } from "@vercel/analytics/react";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
-      <Analytics />
     </ThemeProvider>
   );
 }
